d8e5e50fed2706981d1d6efcbbaa6e0cfb73b713
ggouveia <guilherme.gouveia@iteris.com.br>
2025-02-16 15:44:02 -0300

    creating script to setup the application

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+
+./vendor/bin/sail up -d --build
+./vendor/bin/sail composer install
+
+./vendor/bin/sail artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"
+
+./vendor/bin/sail artisan jwt:secret
+
+./vendor/bin/sail artisan key:generate
+
+./vendor/bin/sail artisan migrate --seed
+
+echo "Setup finished! API available at http://localhost and frontend at http://localhost:4200"
