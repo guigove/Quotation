6053f22ea3ef357d7719d32adae96a6543ed3e10
ggouveia <guilherme.gouveia@iteris.com.br>
2025-02-16 16:02:18 -0300

    Creating readme

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ fi
 ./vendor/bin/sail up -d --build
 ./vendor/bin/sail composer install
 
-./vendor/bin/sail artisan jwt:secret
-
 ./vendor/bin/sail artisan key:generate
 
 ./vendor/bin/sail artisan migrate --seed
+
+./vendor/bin/sail artisan jwt:secret
 
 echo "Setup finished! API available at http://localhost and frontend at http://localhost:4200"
